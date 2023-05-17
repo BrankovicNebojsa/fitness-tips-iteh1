348fd8964665bf18e03a1a455eca9e32497f4b93
Nebojsa Brankovic <n.brankovic99@gmail.com>
2023-05-17 20:52:22 +0200

    Napravljene klase User i Workout

---
+++ model/User.php
@@ -0,0 +1,208 @@
+<?php
+
+class User
+{
+
+    private $id;
+    private $username;
+    private $password;
+    private $email;
+
+    function __construct($username = null, $password = null, $email = null)
+    {
+        $this->id = NULL;
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+    /**
+     * function to log in user
+     */
+    function login(mysqli $conn)
+    {
+
+        $sql = "SELECT id,username,password,email FROM user WHERE username = ? AND password = ?";
+
+        $stmt = $conn->prepare($sql);   // using prepared statement to avoid SQL Injection
+        $stmt->bind_param("ss", $this->username, $this->password);   //s for string
+
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $success = false;
+
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();   //return assoc array, where each element is column with name in table
+
+            $this->id = $row['id'];
+            $this->email = $row['email'];
+
+            $success = true;
+        }
+
+        $stmt->close();
+        return $success;
+    }
+
+
+    /**
+     * function to create in user
+     */
+
+    function create(mysqli $conn)
+    {
+        $sql = "INSERT INTO user (username,password,email) VALUES (?,?,?)";
+
+        $stmt = $conn->prepare($sql);   // using prepared statement to avoid SQL Injection
+        $stmt->bind_param("sss", $this->username, $this->password, $this->email);   //s for string
+
+
+        $success = $stmt->execute();
+
+        $stmt->close();
+
+        return $success;
+    }
+
+    /**
+     * function to update in user
+     */
+
+    function update(mysqli $conn)
+    {
+        $sql = "UPDATE user SET username = ?, password = ?, email = ? WHERE id = ?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sssi", $this->username, $this->password, $this->email, $this->id);
+
+        $success = $stmt->execute();
+
+        $stmt->close();
+
+        return $success;
+    }
+
+    /**
+     * static function to delete in user
+     * trying static functions, it doesn't need to be
+     */
+
+    static function delete(mysqli $conn, $id)
+    {
+        // TODO : Deleting workouts
+
+        $sql = "DELETE FROM workout WHERE user_id = ?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $success = $stmt->execute();
+
+        if (!$success) {
+            throw new Exception("Error deleting user's workouts");
+        }
+
+        $sql = "DELETE FROM user WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+
+        $success = $stmt->execute();
+
+
+        return $success;
+    }
+
+    /**
+     * static function to check if username enterd in registration form, already exists
+     * @param - mysqli connection and entered username
+     * @return - true if username is unique, false  if it is not
+     */
+
+    static function checkUsername(mysqli $conn, $username)
+    {
+        $sql = "SELECT *  FROM user WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+
+        $success = $stmt->execute();
+
+
+        if ($success) {
+
+            $result = $stmt->get_result();
+            return mysqli_num_rows($result) == 0 ? true : false;
+        }
+
+        return false;
+    }
+
+    /**
+     * static function to check if email enterd in registration form, already exists
+     * @param - mysqli connection and entered email
+     * @return - true if email is unique, false  if it is not
+     */
+
+    static function checkEmail(mysqli $conn, $email)
+    {
+        $sql = "SELECT *  FROM user WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+
+        $success = $stmt->execute();
+
+
+        if ($success) {
+
+            $result = $stmt->get_result();
+            return mysqli_num_rows($result) == 0 ? true : false;
+        }
+
+        return false;
+    }
+
+
+
+
+    // getters
+
+    function getUsername()
+    {
+        return $this->username;
+    }
+
+    function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    function getPassword()
+    {
+        return $this->password;
+    }
+
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    // setters
+
+    function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+
+    function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
---
+++ model/Workout.php
@@ -0,0 +1,159 @@
+<?php
+
+class Workout
+{
+
+    public $id;
+    public $name;
+    public $exerciseTime;
+    public $difficultyLevel;
+    public $firstExercise;
+    public $secondExercise;
+    public $thirdExercise;
+    public $description;
+    public $imageName;
+    public $user_id;
+
+    function __construct($id = null, $name, $exerciseTime, $difficultyLevel, $firstExercise, $secondExercise, $thirdExercise, $description, $imageName, $user_id)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->exerciseTime = $exerciseTime;
+        $this->difficultyLevel = $difficultyLevel;
+        $this->firstExercise = $firstExercise;
+        $this->secondExercise = $secondExercise;
+        $this->thirdExercise = $thirdExercise;
+        $this->description =  $description;
+        $this->imageName = $imageName;
+        $this->user_id = $user_id;
+    }
+
+    // function to get all workouts
+    static function getWorkouts(mysqli $conn)
+    {
+        $sql = "SELECT * FROM workout";
+        $result = $conn->query($sql);
+        $array = array();
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $array[] = $row;
+            }
+        }
+
+        return $array;
+    }
+
+    // function to add workout 
+    function insert(mysqli $conn)
+    {
+        $sql = "INSERT INTO workout (name,exercise_time,difficulty_level,first_exercise,second_exercise,third_exercise,description,image,user_id) VALUES (?,?,?,?,?,?,?,?,?)";
+
+        $stmt = $conn->prepare($sql);   // using prepared statement to avoid SQL Injection
+        $stmt->bind_param("siisssssi", $this->name, $this->exerciseTime, $this->difficultyLevel, $this->firstExercise, $this->secondExercise, $this->thirdExercise, $this->description, $this->imageName, $this->user_id);   //s for string
+
+        $success = $stmt->execute();
+
+        $stmt->close();
+
+        return $success;
+    }
+
+    // function to get workout by id
+    static function getById(mysqli $conn, $id)
+    {
+        $sql = "SELECT * FROM workout WHERE id = ?";
+
+        $stmt = $conn->prepare($sql);   // using prepared statement to avoid SQL Injection
+        $stmt->bind_param("i", $id);
+        $success = $stmt->execute();
+
+        if ($success) {
+            $res = $stmt->get_result();
+            $array_res = $res->fetch_array();
+            $workout = new Workout(
+                $array_res['id'],
+                $array_res['name'],
+                $array_res['exercise_time'],
+                $array_res['difficulty_level'],
+                $array_res['first_exercise'],
+                $array_res['second_exercise'],
+                $array_res['third_exercise'],
+                $array_res['description'],
+                $array_res['image'],
+                $array_res['user_id']
+            );
+            return $workout;
+        }
+
+        return false;
+    }
+
+
+    // function to filter workouts from DB
+    static function filterWorkouts(
+        mysqli $conn,
+        $name,
+        $exerciseTimeFrom,
+        $exerciseTimeTo,
+        $difficultyLevelFrom,
+        $difficultyLevelTo
+    ) {
+
+        if (!empty($name) && !Workout::getByName($conn, $name)) {
+            return array();
+        }
+
+
+        if ((!empty($exerciseTimeTo) && $exerciseTimeTo < $exerciseTimeFrom) || (!empty($difficultyLevelTo) && $difficultyLevelTo < $difficultyLevelFrom)) {
+            return array();
+        }
+
+        $sql = "SELECT * FROM workout WHERE 1=1";
+
+        if (!empty($name)) {
+            $name = strtolower($name);
+            $sql .= " AND LOWER(name) LIKE '$name%'";
+        }
+
+        if (!empty($exerciseTimeFrom)) {
+            $exerciseTimeFrom = (int) $exerciseTimeFrom;
+            $sql .= " AND exercise_time >= $exerciseTimeFrom";
+        }
+
+        if (!empty($exerciseTimeTo)) {
+            $exerciseTimeTo = (int) $exerciseTimeTo;
+            $sql .= " AND exercise_time <= $exerciseTimeTo";
+        }
+
+        if (!empty($difficultyLevelFrom)) {
+            $difficultyLevelFrom = (int) $difficultyLevelFrom;
+            $sql .= " AND difficulty_level >= $difficultyLevelFrom";
+        }
+
+
+        if (!empty($difficultyLevelTo)) {
+            $difficultyLevelTo = (int) $difficultyLevelTo;
+            $sql .= " AND difficulty_level <= $difficultyLevelTo";
+        }
+
+        $result = $conn->query($sql);
+        $array = array();
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $array[] = $row;
+            }
+        }
+        return $array;
+    }
+
+    static function getByName(mysqli $conn, $name)
+    {
+        $sql = "SELECT * FROM workout WHERE name LIKE '$name%'";
+        $result = $conn->query($sql);
+        if ($result && $result->num_rows > 0)
+            return true;
+        else
+            return false;
+    }
+}
